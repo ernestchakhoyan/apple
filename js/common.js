2db56937cae68e74c319b7ab5b7dbaba1098801c
Ernest Chakhoyan <chakhoyanernest@gmail.com>
2016-07-16 23:37:22 +0400

    section-IphonePics

---
+++ js/common.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+/*Menu*************************************************************************/
+
+    var navPos, winPos, navHeight;
+
+    function refreshVar() {
+        navPos = $('.top-line').offset().top;
+        navHeight = $('.top-line').outerHeight(true);
+    }
+
+    refreshVar();
+    $(window).resize(refreshVar);
+
+    $('<div class="clone-nav"></div>').insertBefore('.top-line').css('height', navHeight).hide();
+
+    $(window).scroll(function () {
+        winPos = $(window).scrollTop();
+
+        if (winPos >= navPos) {
+            $('.top-line').addClass('fixed shadow');
+            $('.clone-nav').show();
+        } else {
+            $('.top-line').removeClass('fixed shadow');
+            $('.clone-nav').hide();
+        }
+    });
+
+/**********************************************************************************/
+
+});
